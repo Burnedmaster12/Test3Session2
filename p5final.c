e0f18621fb16d62f19ed6ceb93e1827186cb1e91
Faizal Hussain <96277916+Burnedmaster12@users.noreply.github.com>
2022-03-19 17:53:09 +0000

    done p5 final

--- p5final.c
+++ p5final.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 int input()
 {
   int n;
@@ -18,7 +18,7 @@ void ets(int n, int a[n])
 {
   int i=0;
    /* Find next non-zero number */
-  while(i<sqrt(n)){
+  /*while(i<sqrt(n)){
    for(;a[i]==0 ;i++);
    for(int k=i+i;k<n;k += i)
      a[i] = 0;
@@ -43,5 +43,43 @@ int main()
   init_array(n,a);
   ets(n,a);
   display(n,a);
+  return 0;
+}*/
+#include<stdio.h>
+int input_array_size()
+{
+  int n;
+  printf("enter the array size\n");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n, int a[n])
+{ int i;  
+  for(i=0;i<n;i++)
+  {
+  printf("enter the array element %d\n",i);
+  scanf("%d",&a[i]);
+  }
+}
+int find_largest(int n, int a[n])
+{ 
+  int i,largest=0;
+  for(i = 0;i<n;i++)
+   if (a[i] > a[largest])
+      largest = i;
+  return largest;
+}
+void out_put(int n, int a[n], int largest)
+{
+printf("Largest element is present at index %d and its value is %d.\n",largest,a[largest]);
+}
+int main()
+{
+  int largest,n;
+  n=input_array_size();
+  int a[n];
+  input_array(n,a);
+  largest=find_largest(n,a);
+  out_put(n,a,largest);
   return 0;
 }
